9b90cc41c49f70be9d154f59a5abb41875ea4bee
Madok <mkdormenyo@st.ug.edu.gh>
2025-07-10 19:10:29 +0000

    First commit

---
+++ first.py
@@ -0,0 +1,5 @@
+A = int(input("Enter the size of the array: "))
+if A   0:
+    print("Array size must be greater than Zero"),
+else:
+    print(f'{A}')
---
+++ second.py
@@ -0,0 +1,7 @@
+A = []
+B = int(input("Enter the size of the array: "))
+if B  <= 0:
+    print("Array size must be greater than Zero"),
+else:
+    print(f'The size of the array is {B}')
+
